7bc6d44188d782cd470e9800262102900cfb79c7
Aliice77 <69522330+Aliice77@users.noreply.github.com>
2022-06-05 22:45:24 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace new1
+{
+    public  class classshape{
+        public int Num3 { get; set; }
+        public classshape(int num,int num2){
+            Num3=num2*num; 
+            Console.WriteLine("masahat barabar ast ba: "+Num3);
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("tol ra vared konid: ");
+            int a=Convert.ToInt32( Console.ReadLine());
+            Console.Write("arz ra vared konid: ");
+            int b=Convert.ToInt32( Console.ReadLine());
+            classshape cl=new classshape(a,b);
+        }
+    }
+}
+}
